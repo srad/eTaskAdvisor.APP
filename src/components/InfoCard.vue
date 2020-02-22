eee1507f0cc29c1b7fb7e5c35fc1c6205c6dffe0
srad <ssedighi@posteo.de>
2020-02-22 22:57:07 +0100

    Improving InfoCard.vue, optional footer rendering.

--- src/components/InfoCard.vue
+++ src/components/InfoCard.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
-    <b-card class="mb-2 shadow-sm" border-variant="secondary" header-tag="header" footer-tag="footer" header-bg-variant="secondary" header-class="text-white p-2" footer-class="p-1 text-right">
+    <b-card class="mb-2 shadow-sm" border-variant="secondary" header-tag="header" footer-tag="footer" header-bg-variant="secondary" body-class="p-2" header-class="text-white p-1 align-middle" footer-class="p-1 text-right">
       <template v-slot:header>
-        <h6 class="mb-0">
+        <h6 class="float-left m-0 p-1">
           <slot name="header"></slot>
         </h6>
+        <b-button class="float-right m-0 shadow-sm" @click="$emit('destroy', myObj)" size="sm" variant="danger">Delete</b-button>
       </template>
       <b-card-text>
         <slot name="content"></slot>
       </b-card-text>
-      <template v-slot:footer>
-        <b-button @click="$emit('destroy', myObj)" size="sm" variant="danger">Delete</b-button>
-      </template>
+      <b-card-footer v-if="$slots['footer']" footer-class="p-1" footer-bg-variant="transparent">
+        <slot name="footer"/>
+      </b-card-footer>
     </b-card>
   </div>
 </template>
