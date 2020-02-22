4a619cc9b1feab79884f6545bc425df1dc449a23
srad <ssedighi@posteo.de>
2020-02-22 23:02:37 +0100

    Updating About.vue, removing obsolete Profile.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,30 +1,14 @@
 <template>
   <div class="about">
-    <p>Mobile PWA ....</p>
-    <hr/>
-    <h3>Used frameworks and libs</h3>
-    <ol>
-      <li v-for="[link, label] in links" :key="link">
-        <a :href="link" v-text="label" target="_blank"></a>
-      </li>
-    </ol>
+    <p>The purpose of this application is to gather research on the influence of environmental
+      effect on learning activities and making them easily accessible.</p>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-      links: [
-        ["https://vuejs.org/", "Vue.js"],
-        ["https://bootstrap-vue.js.org/", "BootstrapVue"],
-        ["https://vuex.vuejs.org/", "Vuex"],
-        ["https://expressjs.com/", "Express"],
-        ["https://fontawesome.com/", "Font Awesome"],
-        ["https://github.com/axios/axios", "axios"],
-        ["https://www.chaijs.com/", "Chai"],
-      ],
-    };
+    return {};
   },
 };
 </script>
