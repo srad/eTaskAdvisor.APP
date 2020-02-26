1a7a434ed507c9ea92e343828d7a818dafefb63f
srad <ssedighi@posteo.de>
2020-02-26 17:18:45 +0100

    Testing google sign in component.

--- src/router.js
+++ src/router.js
@@ -7,12 +7,14 @@ import Knowledge from "./views/Knowledge.vue";
 import About from "./views/About.vue";
 import Guide from "./views/Guide";
 import Iframe from "./components/Iframe.vue";
+import SignIn from "./views/SignIn";
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {path: "/", name: "Home", component: Home, meta: {title: "Home", header: false, poster: true}},
+    {path: "/sign-in", name: "SignIn", component: SignIn, meta: {title: "Sign In", header: true, poster: true}},
     {path: "/tasks/:activityId?", name: "Tasks", component: Tasks, meta: {title: "Learning Tasks", poster: true}},
     {path: "/guide", name: "Guide", component: Guide, meta: {title: "Guide", header: false, poster: true}},
     {path: "/knowledge", name: "Knowledge", component: Knowledge, meta: {title: "Knowledge", poster: true}},
---
+++ src/views/SignIn.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div id="googleLogin" class="float-left"></div>
+    <b-button class="float-right" style="background: #4285f4" size="md" @click="signOut">Sign Out</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignIn",
+  data() {
+    return {};
+  },
+  methods: {
+    signOut() {
+      this.auth2.signOut();
+    },
+  },
+  mounted() {
+    window.gapi.load("auth2,signin2", () => {
+      window.gapi.signin2.render("googleLogin", {
+        "scope": "profile email",
+        "longtitle": true,
+        "theme": "dark",
+      });
+
+      const auth2 = window.gapi.auth2.getAuthInstance();
+      this.auth2 = auth2;
+
+      auth2.isSignedIn.listen((signedIn) => {
+        this.$log("signIn", signedIn);
+      });
+      auth2.currentUser.listen((user) => {
+        this.$log("currentUser", user);
+        if (user.isSignedIn()) {
+          this.$log(user.BasicProfile());
+          //  console.log(auth2.signIn());
+        }
+      });
+    });
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
