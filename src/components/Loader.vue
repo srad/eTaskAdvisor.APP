f531d39e4fe413d41fd834e9b2cb97b938fbda14
srad <ssedighi@posteo.de>
2020-02-26 10:35:25 +0100

    Adding Loader.vue component

---
+++ src/components/Loader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-center" v-if="loading">
+    <b-spinner style="width: 3rem; height: 3rem;" variant="primary" :label="label || 'Loading'"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Loader",
+  props: {
+    label: String,
+    loading: Boolean,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
